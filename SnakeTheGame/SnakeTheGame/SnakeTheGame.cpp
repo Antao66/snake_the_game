3861ecab114497238a16898b4a636432cc1662a2
Antao66 <46621433+Antao66@users.noreply.github.com>
2021-03-28 19:18:56 +0300

    add SnakeTheGame sln, add renderPlayer  func and arial.ttf

---
+++ SnakeTheGame/SnakeTheGame/SnakeTheGame.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <SDL.h> 
+#include <SDL_ttf.h> 
+
+using namespace std;
+
+void renderPlayer(SDL_Renderer* renderer, SDL_Rect player, int x, int y, int scale, vector<int> tailX, vector<int> tailY, int tailLength)
+{
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	player.w = scale;
+	player.h = scale;
+
+	// Отримує x та y всіх хвостів блоків та відображає їх
+	for (int i = 0; i < tailLength; i++)
+	{
+		player.x = tailX[i];
+		player.y = tailY[i];
+		SDL_RenderFillRect(renderer, &player);
+	}
+
+	player.x = x;
+	player.y = y;
+
+	SDL_RenderFillRect(renderer, &player);
+}
+

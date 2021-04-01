e7ece6c424f7b3e536cb199b763921fd2a67e1d3
Antao66 <46621433+Antao66@users.noreply.github.com>
2021-04-01 22:02:43 +0300

    add renderFood and renderScore

--- SnakeTheGame/SnakeTheGame/SnakeTheGame.cpp
+++ SnakeTheGame/SnakeTheGame/SnakeTheGame.cpp
@@ -13,7 +13,7 @@ void renderPlayer(SDL_Renderer* renderer, SDL_Rect player, int x, int y, int sca
 	player.w = scale;
 	player.h = scale;
 
-	// Отримує x та y всіх хвостів блоків та відображає їх
+	
 	for (int i = 0; i < tailLength; i++)
 	{
 		player.x = tailX[i];
@@ -27,3 +27,30 @@ void renderPlayer(SDL_Renderer* renderer, SDL_Rect player, int x, int y, int sca
 	SDL_RenderFillRect(renderer, &player);
 }
 
+void renderFood(SDL_Renderer* renderer, SDL_Rect food)
+{
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	SDL_RenderFillRect(renderer, &food);
+}
+
+void renderScore(SDL_Renderer* renderer, int tailLength, int scale, int wScale)
+{
+	SDL_Color Black = { 0, 0, 0 };
+
+	TTF_Font* font = TTF_OpenFont((char*)"arial.ttf", 10);
+	if (font == NULL) {
+		cout << "Font loading error" << endl;
+		return;
+	}
+
+	SDL_Surface* score = TTF_RenderText_Solid(font, (string("Score: ") + to_string(tailLength * 10)).c_str(), Black);
+	SDL_Texture* scoreMessage = SDL_CreateTextureFromSurface(renderer, score);
+	SDL_Rect scoreRect;
+	scoreRect.w = 100;
+	scoreRect.h = 30;
+	scoreRect.x = ((scale * wScale) / 2) - (scoreRect.w / 2);
+	scoreRect.y = 0;
+	SDL_RenderCopy(renderer, scoreMessage, NULL, &scoreRect);
+
+	TTF_CloseFont(font);
+}
